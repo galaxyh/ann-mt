a0899c6cec3d6225f35c65f0c703adecf4ba8248
Yu-chun Huang <code@yullage.com>
2016-03-23 13:10:59 +0800

    Enable batch training

--- attention_seq2seq.py
+++ attention_seq2seq.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import time
-import numpy as np
 import word2vec
 import corpus_processor as cp
 import config as cfg
@@ -13,46 +12,8 @@ from utils import get_logger
 _logger = get_logger(__name__)
 
 
-def word_to_one_hot(w2vm, word):
-    v = np.zeros(len(w2vm.vocab))
-    v[w2vm.vocab[word].index] = 1.0
-    return v
-
-
-def one_hot_to_word(w2vm, word_vec):
-    return w2vm.index2word[np.argmax(word_vec)]
-
-
-def sentence_to_one_hot(maxlen, w2vm, sentence):
-    s = np.full((maxlen, len(w2v_model.vocab)), word_to_one_hot(w2vm, cfg.EOS_SYMBOL))
-    for i, t in enumerate(sentence):
-        s[i] = word_to_one_hot(w2vm, t)
-    return s
-
-
-def one_hot_to_sentence(w2vm, sentence_vec):
-    return [one_hot_to_word(w2vm, wv) for wv in sentence_vec]
-
-
-def get_one_hot_ndarray(sentences, max_sent_len, w2vm):
-    out_ndarray = np.empty((len(sentences), max_sent_len, len(w2vm.vocab)))
-    for i, s in enumerate(sentences):
-        out_ndarray[i] = sentence_to_one_hot(max_sent_len, w2vm, s)
-    return out_ndarray
-
-
-def train_test(nn_params, train_pair, test_pair):
-    (x_train, y_train) = train_pair
-    (x_test, y_test) = test_pair
-
-    _logger.info('x_train shape: {}'.format(x_train.shape))
-    _logger.info('y_train shape: {}'.format(y_train.shape))
-    _logger.info('x_test shape: {}'.format(x_test.shape))
-    _logger.info('y_test shape: {}'.format(y_test.shape))
-
-    ts0 = time.time()
-    ts1 = time.time()
-
+def build_nn_model(nn_params):
+    ts = time.time()
     _logger.info('Building NN model...')
     model = AttentionSeq2seq(input_dim=nn_params['input_dim'],
                              input_length=nn_params['input_length'],
@@ -62,24 +23,24 @@ def train_test(nn_params, train_pair, test_pair):
                              depth=nn_params['depth'])
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    _logger.info('Done building NN model ({:.1f} minutes).'.format((time.time() - ts1) / 60))
-
-    ts1 = time.time()
-
-    _logger.info('Training...')
-    model.fit(x_train, y_train, nb_epoch=nn_params['nb_epoch'], show_accuracy=True)
-    _logger.info('Done training ({:.1f} minutes).'.format((time.time() - ts1) / 60))
-
-    ts1 = time.time()
-
-    _logger.info('Evaluating...')
-    objective_score = model.evaluate(x_test, y_test)
-    _logger.info('Objective score = {}'.format(objective_score))
-    _logger.info('Done evaluation ({:.1f} minutes)'.format((time.time() - ts1) / 60))
-
-    _logger.info('Total time elapsed: {:.1f} minutes.'.format((time.time() - ts0) / 60))
-
+    _logger.info('Done building NN model ({:.1f} minutes).'.format((time.time() - ts) / 60))
     return model
+
+
+def train(nn_model, batch_iter):
+    ts = time.time()
+    _logger.info('Training...')
+    for array_s, array_t in batch_iter:
+        nn_model.fit(array_s, array_t, nb_epoch=1, batch_size=len(array_s), show_accuracy=True)
+    _logger.info('Done training ({:.1f} minutes).'.format((time.time() - ts) / 60))
+
+
+def test(nn_model, test_batch):
+    ts = time.time()
+    _logger.info('Evaluating...')
+    objective_score = nn_model.evaluate(test_batch)
+    _logger.info('Objective score = {}'.format(objective_score))
+    _logger.info('Done evaluation ({:.1f} minutes)'.format((time.time() - ts) / 60))
 
 
 def save_model(nn_model, nn_params, corpus_name):
@@ -117,39 +78,25 @@ if __name__ == '__main__':
     w2v_model = word2vec.get_model(w2v_params, sentences_all, rebuild=cfg.W2V_REBUILD_MODEL)
     _logger.info('Done building word2vec model ({:.1f} minutes)'.format((time.time() - ts0) / 60))
 
-    _logger.info('Preparing train/test data...')
-    ts0 = time.time()
-    sent_train_s, sent_train_t = cp.get_parallel_processed_sentences(w2v_model.vocab,
-                                                                     corpus_train_source,
-                                                                     corpus_train_target,
-                                                                     maxlen_a=maxlen_source,
-                                                                     maxlen_b=maxlen_target)
-
-    x_train = get_one_hot_ndarray(sent_train_s, maxlen_source, w2v_model)
-    y_train = get_one_hot_ndarray(sent_train_t, maxlen_target, w2v_model)
-
-    sent_test_s, sent_test_t = cp.get_parallel_processed_sentences(w2v_model.vocab,
-                                                                   corpus_test_source,
-                                                                   corpus_test_target,
-                                                                   maxlen_a=maxlen_source,
-                                                                   maxlen_b=maxlen_target)
-
-    x_test = get_one_hot_ndarray(sent_test_s, maxlen_source, w2v_model)
-    y_test = get_one_hot_ndarray(sent_test_t, maxlen_target, w2v_model)
-    _logger.info('Done preparing train/test data ({:.1f} minutes)'.format((time.time() - ts0) / 60))
-
-    nn_param = {'nb_epoch': cfg.NN_EPOCH_NUM,
-                'input_dim': x_train.shape[2],
-                'input_length': x_train.shape[1],
+    nn_params = {'nb_epoch': cfg.NN_EPOCH_NUM,
+                'input_dim': len(w2v_model.vocab),
+                'input_length': cfg.NN_SENTENCE_MAX_LENGTH_SOURCE,
                 'hidden_dim': cfg.NN_HIDDEN_DIM,
-                'output_length': y_train.shape[1],
-                'output_dim': y_train.shape[2],
+                'output_length': cfg.NN_SENTENCE_MAX_LENGTH_TARGET,
+                'output_dim': len(w2v_model.vocab),
                 'depth': cfg.NN_DEPTH}
+    nn_model = build_nn_model(nn_params)
 
-    attention_model = train_test(nn_param, (x_train, y_train), (x_test, y_test))
-    save_model(attention_model)
+    one_hot_iter = cp.get_parallel_one_hot_ndarray_iter(w2v_model.vocab,
+                                                     cfg.NN_BATCH_SIZE,
+                                                     corpus_train_source,
+                                                     corpus_train_target,
+                                                     maxlen_a=maxlen_source,
+                                                     maxlen_b=maxlen_target)
+    train(nn_model, one_hot_iter)
+    save_model(nn_model, nn_params, cfg.NN_CORPUS_NAME)
 
-    _logger.info('Prediction and ground truth:')
-    for p, g in zip(attention_model.predict(x_test), y_test):
+    '''_logger.info('Prediction and ground truth:')
+    for p, g in zip(nn_model.predict(x_test), y_test):
         _logger.info('[P] {}'.format(one_hot_to_sentence(w2v_model, p)))
-        _logger.info('[G] {}'.format(one_hot_to_sentence(w2v_model, g)))
+        _logger.info('[G] {}'.format(one_hot_to_sentence(w2v_model, g)))'''
--- corpus_processor.py
+++ corpus_processor.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import codecs
+import numpy as np
 
 from config import EOS_SYMBOL, UNK_SYMBOL
 from utils import get_logger
@@ -43,7 +44,7 @@ def get_parallel_sentences_iter(corpus_a, corpus_b, maxlen_a=MAX_INT_VALUE, maxl
             yield sent_a, sent_b
 
 
-def get_parallel_sentences(corpus_a, corpus_b, maxlen_a=MAX_INT_VALUE, maxlen_b=MAX_INT_VALUE):
+def get_parallel_sentences_all(corpus_a, corpus_b, maxlen_a=MAX_INT_VALUE, maxlen_b=MAX_INT_VALUE):
     all_a = []
     all_b = []
     for (sent_a, sent_b) in get_parallel_sentences_iter(corpus_a, corpus_b, maxlen_a=maxlen_a, maxlen_b=maxlen_b):
@@ -64,12 +65,72 @@ def get_parallel_processed_sentences_iter(vocab, corpus_a, corpus_b, maxlen_a=MA
             yield sent_a, sent_b
 
 
-def get_parallel_processed_sentences(vocab, corpus_a, corpus_b, maxlen_a=MAX_INT_VALUE, maxlen_b=MAX_INT_VALUE):
+def get_parallel_processed_sentences_all(parallel_sentence_iter):
     all_a = []
     all_b = []
-    for (sent_a, sent_b) in get_parallel_processed_sentences_iter(vocab,
-                                                                  corpus_a, corpus_b,
-                                                                  maxlen_a=maxlen_a, maxlen_b=maxlen_b):
+    for (sent_a, sent_b) in parallel_sentence_iter:
         all_a.append(sent_a)
         all_b.append(sent_b)
     return all_a, all_b
+
+
+def _get_parallel_batch(parallel_sentences, batch_size=1):
+    batch = []
+    for s_a, s_b in parallel_sentences:
+        batch.append((s_a, s_b))
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    yield []
+
+
+def get_parallel_processed_sentences_batch_iter(parallel_sentence_iter, batch_size=1):
+    for parallel_batch in _get_parallel_batch(parallel_sentence_iter, batch_size):
+        batch_a = []
+        batch_b = []
+        if parallel_batch:
+            for pb in parallel_batch:
+                batch_a.append(pb[0])
+                batch_b.append(pb[1])
+        yield batch_a, batch_b
+
+
+def word_to_one_hot(vocab, word):
+    v = np.zeros(len(vocab))
+    v[vocab[word].index] = 1.0
+    return v
+
+
+def one_hot_to_word(index2word, word_vec):
+    return index2word[np.argmax(word_vec)]
+
+
+def sentence_to_one_hot(maxlen, vocab, sentence):
+    s = np.full((maxlen, len(vocab)), word_to_one_hot(vocab, EOS_SYMBOL))
+    for i, t in enumerate(sentence):
+        s[i] = word_to_one_hot(vocab, t)
+    return s
+
+
+def one_hot_to_sentence(index2word, sentence_vec):
+    return [one_hot_to_word(index2word, wv) for wv in sentence_vec]
+
+
+def get_one_hot_ndarray(sentences, max_sent_len, vocab):
+    out_ndarray = np.empty((len(sentences), max_sent_len, len(vocab)))
+    for i, s in enumerate(sentences):
+        out_ndarray[i] = sentence_to_one_hot(max_sent_len, vocab, s)
+    return out_ndarray
+
+
+def get_parallel_one_hot_ndarray_iter(vocab, batch_size,
+                                      corpus_a, corpus_b,
+                                      maxlen_a=MAX_INT_VALUE, maxlen_b=MAX_INT_VALUE):
+    parallel_iter = get_parallel_processed_sentences_iter(vocab,
+                                                          corpus_a, corpus_b,
+                                                          maxlen_a=maxlen_a, maxlen_b=maxlen_b)
+    batch_iter = get_parallel_processed_sentences_batch_iter(parallel_iter, batch_size=batch_size)
+    for batch_s, batch_t in batch_iter:
+        array_s = get_one_hot_ndarray(batch_s, maxlen_a, vocab)
+        array_t = get_one_hot_ndarray(batch_t, maxlen_b, vocab)
+        yield array_s, array_t
