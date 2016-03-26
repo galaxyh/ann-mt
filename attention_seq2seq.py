7b1b52df81c8ce4693ef1cae26246845b554f32c
Yu-chun Huang <code@yullage.com>
2016-03-26 20:47:59 +0800

    Bug fix

--- attention_seq2seq.py
+++ attention_seq2seq.py
@@ -144,7 +144,8 @@ if __name__ == '__main__':
         if source == '__EOT__':
             break
         source = source.split()
-        x = cp.sentence_to_one_hot(cfg.NN_SENTENCE_MAX_LENGTH_SOURCE, w2v_model.vocab, source)
-        for target in nn_model.predict([x]):
+        x = cp.get_one_hot_ndarray([source], cfg.NN_SENTENCE_MAX_LENGTH_SOURCE, w2v_model.vocab)
+        for target in nn_model.predict(x):
             ch = cp.one_hot_to_sentence(w2v_model.index2word, target)
+            print('CH> ' + ch)
             print('CH> ' + ''.join(ch))
